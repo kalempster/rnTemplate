9dac7c378637ffa4d2f2040cb35c9abfef60f91c
kalempster <mrpiter1337@gmail.com>
2023-05-09 22:44:17 +0200

    Minor fixes

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     content: [
         "./App.{js,jsx,ts,tsx}",
-        "./<custom-folder>/**/*.{js,jsx,ts,tsx}"
+        "./src/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
         extend: {}
